a6f2df28bb254fb3fd24fcf345b68a816cdb4e78
sushanttsharma0011 <135987312+sushanttsharma0011@users.noreply.github.com>
2024-02-12 00:17:33 +0545

    changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sushantsharmaofficial",
   resume:
-    "https://drive.google.com/file/d/1abCjuzpXxw0p-a2rafLV6dqtFA4ka2Dp/view?usp=sharing",
+    "https://drive.google.com/file/d/1rugwqD8uWtICLE6fe5uhbE-W8IRaT-Gi/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sushant-sharma-152244230/",
   twitter: "https://twitter.com/SushantSharrma",
   insta: "https://www.instagram.com/sushant_sharrma/",
